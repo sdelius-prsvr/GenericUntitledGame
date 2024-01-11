43e24d5198b96c12edd724cadb0bc53d6caa89cb
stephen <sdelius.prsvr@gmail.com>
2024-01-10 22:39:38 -0600

    added main.js as deferred script to index.html; moved viewer css to index.html; added p to dialogue box; moved rNG function to top of main.js; created bgFunc to change viewer background image based on rNG and location; added onclick functions to location buttons; created todo.txt

--- main.js
+++ main.js
@@ -1,51 +1,3 @@
-// constants for menu buttons
-const cluesBtn = document.querySelector('#cluesBtn');
-cluesBtn.onclick = cluesFunc
-const itemsBtn = document.querySelector('#itemsBtn');
-itemsBtn.onclick = itemsFunc
-const notesBtn = document.querySelector('#notesBtn');
-notesBtn.onclick = notesFunc
-
-// constants for action buttons
-const enterBtn = document.querySelector('#enterBtn');
-enterBtn.onclick = enterFunc
-const fleeBtn = document.querySelector('#fleeBtn');
-fleeBtn.onclick = fleeFunc
-const itemBtn = document.querySelector('#itemBtn');
-itemBtn.onclick = itemFunc
-const searchBtn = document.querySelector('#searchBtn');
-searchBtn.onclick = searchFunc
-const talkBtn = document.querySelector('#talkBtn');
-talkBtn.onclick = talkFunc
-
-// constants for location buttons
-const castleBtn = document.querySelector('#castleBtn');
-castleBtn.onclick = bgFunc(castle)
-const churchBtn = document.querySelector('#churchBtn');
-churchBtn.onclick = churchFunc
-const estateBtn = document.querySelector('#estateBtn');
-estateBtn.onclick = estateFunc
-const forestBtn = document.querySelector('#forestBtn');
-forestBtn.onclick = forestFunc
-const gardenBtn = document.querySelector('#gardenBtn');
-gardenBtn.onclick = gardenFunc
-const greenhouseBtn = document.querySelector('#greenhouseBtn');
-greenhouseBtn.onclick = greenhouseFunc
-const hutBtn = document.querySelector('#hutBtn');
-hutBtn.onclick = hutFunc
-const lakeBtn = document.querySelector('#lakeBtn');
-lakeBtn.onclick = lakeFunc
-const libraryBtn = document.querySelector('#libraryBtn');
-libraryBtn.onclick = libraryFunc
-const lighthouseBtn = document.querySelector('#lighthouseBtn');
-lighthouseBtn.onclick = lighthouseFunc
-const mountainBtn = document.querySelector('#mountainBtn');
-mountainBtn.onclick = mountainFunc
-const riverBtn = document.querySelector('#riverBtn');
-riverBtn.onclick = riverFunc
-const towerBtn = document.querySelector('#towerBtn');
-towerBtn.onclick = towerFunc
-
 // rng function to be used by other functions
 
 function rNG(min, max) {
@@ -54,8 +6,101 @@ function rNG(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// location function to change background to random image of type
+// constants for page elements
+const viewer = document.querySelector('#viewer');
+const dialogueBox = document.querySelector('#dialogueBox')
 
-const bgFunc(loc) {
-    
+// constants for menu buttons
+const cluesBtn = document.querySelector('#cluesBtn');
+const itemsBtn = document.querySelector('#itemsBtn');
+const notesBtn = document.querySelector('#notesBtn');
+
+// constants for action buttons
+const enterBtn = document.querySelector('#enterBtn');
+const fleeBtn = document.querySelector('#fleeBtn');
+const itemBtn = document.querySelector('#itemBtn');
+const searchBtn = document.querySelector('#searchBtn');
+const talkBtn = document.querySelector('#talkBtn');
+
+// constants for location buttons
+const castleBtn = document.querySelector('#castleBtn');
+const churchBtn = document.querySelector('#churchBtn');
+const estateBtn = document.querySelector('#estateBtn');
+const forestBtn = document.querySelector('#forestBtn');
+const gardenBtn = document.querySelector('#gardenBtn');
+const greenhouseBtn = document.querySelector('#greenhouseBtn');
+const hutBtn = document.querySelector('#hutBtn');
+const lakeBtn = document.querySelector('#lakeBtn');
+const libraryBtn = document.querySelector('#libraryBtn');
+const lighthouseBtn = document.querySelector('#lighthouseBtn');
+const mountainBtn = document.querySelector('#mountainBtn');
+const riverBtn = document.querySelector('#riverBtn');
+const towerBtn = document.querySelector('#towerBtn');
+
+// function to change background
+const bgFunc = (curLoc, min, max) => {
+    let rNum = rNG(min,max);
+    viewer.style.backgroundImage=`url(assets/backgrounds/${curLoc}/${curLoc}${rNum}.jpeg)`;
+    return rNum;
+}
+
+const preNumSet =  []
+
+const dialogueFunc = (curLoc, curNum) => {
+
+}
+
+// functions for location buttons
+castleBtn.onclick = () => {
+    dialogueBox.innerHTML='<p>A castle rises before you.</p>';
+    console.log(bgFunc('castle',1,7));
+    dialogueBox.p.innerText='You stand before a castle.';
+}
+
+churchBtn.onclick = () => {
+    bgFunc('church',1,3);
+}
+
+estateBtn.onclick = () => {
+    bgFunc('estate',1,5);
+}
+
+forestBtn.onclick = () => {
+    bgFunc('forest',1,8);
+}
+
+gardenBtn.onclick = () => {
+    bgFunc('garden',1,6);
+}
+
+greenhouseBtn.onclick = () => {
+    bgFunc('greenhouse',1,6);
+}
+
+hutBtn.onclick = () => {
+    bgFunc('hut',1,12);
+}
+
+lakeBtn.onclick = () => {
+    bgFunc('lake',1,7);
+}
+
+libraryBtn.onclick = () => {
+    bgFunc('library',1,4);
+}
+
+lighthouseBtn.onclick = () => {
+    bgFunc('lighthouse',1,4);
+}
+
+mountainBtn.onclick = () => {
+    bgFunc('mountain',1,5);
+}
+
+riverBtn.onclick = () => {
+    bgFunc('river',1,13);
+}
+
+towerBtn.onclick = () => {
+    bgFunc('tower',1,7);
 }
