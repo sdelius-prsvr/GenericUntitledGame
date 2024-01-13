bbeb2fa1730199a7e480d581a3dd721b9fc6456e
stephen <sdelius.prsvr@gmail.com>
2024-01-12 21:49:28 -0600

    created first location object to store bg url, nav menu button labels and functions; working on location function to read objects and update nav menu and dialogue; next will be menu buttons and item, clue, and note objects, and an action menu that updates based on item and location objects

--- main.js
+++ main.js
@@ -9,19 +9,19 @@ function rNG(min, max) {
 // constants for page elements
 const viewer = document.querySelector('#viewer');
 const dialogueBox = document.querySelector('#dialogueBox');
-const dialogueText = document.querySelector('#dialogueBox');
 
 // constants for menu buttons
-const cluesBtn = document.querySelector('#cluesBtn');
-const itemsBtn = document.querySelector('#itemsBtn');
-const notesBtn = document.querySelector('#notesBtn');
+const menuBtn1 = document.querySelector('#menuBtn1');
+const menuBtn2 = document.querySelector('#menuBtn2');
+const menuBtn3 = document.querySelector('#menuBtn3');
+const menuBtn4 = document.querySelector('#menuBtn4');
 
 // constants for action buttons
-const enterBtn = document.querySelector('#enterBtn');
-const fleeBtn = document.querySelector('#fleeBtn');
-const itemBtn = document.querySelector('#itemBtn');
-const searchBtn = document.querySelector('#searchBtn');
-const talkBtn = document.querySelector('#talkBtn');
+const actBtn1 = document.querySelector('#actBtn1');
+const actBtn2 = document.querySelector('#actBtn2');
+const actBtn3 = document.querySelector('#actBtn3');
+const actBtn4 = document.querySelector('#actBtn4');
+
 
 // constants for navigation buttons
 const navBtn1 = document.querySelector('#navBtn1');
@@ -29,30 +29,16 @@ const navBtn2 = document.querySelector('#navBtn2');
 const navBtn3 = document.querySelector('#navBtn3');
 const navBtn4 = document.querySelector('#navBtn4');
 
-// const castleBtn = document.querySelector('#castleBtn');
-// const churchBtn = document.querySelector('#churchBtn');
-// const estateBtn = document.querySelector('#estateBtn');
-// const forestBtn = document.querySelector('#forestBtn');
-// const gardenBtn = document.querySelector('#gardenBtn');
-// const greenhouseBtn = document.querySelector('#greenhouseBtn');
-// const hutBtn = document.querySelector('#hutBtn');
-// const lakeBtn = document.querySelector('#lakeBtn');
-// const libraryBtn = document.querySelector('#libraryBtn');
-// const lighthouseBtn = document.querySelector('#lighthouseBtn');
-// const mountainBtn = document.querySelector('#mountainBtn');
-// const riverBtn = document.querySelector('#riverBtn');
-// const towerBtn = document.querySelector('#towerBtn');
-
 // function to change background
-const bgFunc = (curLoc, min, max) => {
-    let rNum = rNG(min,max);
-    viewer.style.backgroundImage=`url(assets/backgrounds/${curLoc}/${curLoc}${rNum}.jpeg)`;
-    return rNum;
-}
+// viewer.style.backgroundImage=`${obj.bgImgUrl}`;
 
-const preNumSet =  []
+forest1 = {}
 
-let i = 0;
+forest2 = {}
+
+forest3 = {}
+
+let i = 0
 let txt = '';
 let speed = 35;
 
@@ -68,10 +54,9 @@ const dialogueFunc = () => {
 // functions for navigation buttons
 const nMtnFunc = () => {
     document.getElementById("dialogueBox").innerHTML = '';
-    i = 0
     txt = "The forest path has brought you to a clearing where sits an old castle.  Its battlements are worn and it's a little overgrown, but it looks habitable, possibly even inhabited.";
     console.log(bgFunc('nMtn1',1,3));
-    dialogueFunc();
+    dialogueFunc(txt, 35);
 }
 
 const forest1Func = () => {
@@ -90,89 +75,45 @@ const sMtnFunc = () => {
     dialogueFunc();
 }
 
-// null.onclick = () => {
-//     document.getElementById("dialogueBox").innerHTML = '';
-//     i = 0
-//     txt = "The forest path has brought you to a clearing where sits an old castle.  Its battlements are worn and it's a little overgrown, but it looks habitable, possibly even inhabited.";
-//     console.log(bgFunc('castle',1,3));
-//     dialogueFunc();
+
+let statueObj = {
+    navBtnLabels : {
+        1 : 'N: Mountains',
+        2 : 'W: Forest',
+        3 : 'S: Mountains',
+        4 : ''
+    },
+    navBtnFuncs : {
+        1 : 'nMtnFunc',
+        2 : 'forest1Func',
+        3 : 'sMtnFunc',
+        4 : ''
+    },
+    dialogueTxt : 'You find yourself standing on a dirt road in front of an admittedly odd statue of an angel. The road heads into the mountains to the north and the south. Opposite the statue, a path leads into a large forest to the west.  The rest of this message is just for testing the size of this textbox.  Hopefully it will be large enough to contain everything I need to say.'
+
+}
+
+// const locaFunc = (obj) => {
+//     for btn of obj.navBtnLabels {
+
+//     }
 // }
 
-
 const statueFunc = () =>{
-    navBtn1.innerHTML = 'N. mountains';
-    navBtn1.onclick = nMtnFunc;
-    navBtn2.innerHTML = 'W. forest';
-    navBtn2.onclick = forest1Func;
-    navBtn3.innerHTML = 'S. mountains';
-    navBtn3.onclick = sMtnFunc;
-    navBtn4.innerHTML = '';
+    for (let i = 1; i < 4; i++)
+	navBtn${i}.innerHTML = statueObj.navBtnLabels[i];
+    // navBtn1.innerHTML = statueObj.navBtnLabels[1];
+    // navBtn1.onclick = statueObj.navBtnFuncs[1];
+    // navBtn2.innerHTML = 'W. forest';
+    // navBtn2.onclick = forest1Func;
+    // navBtn3.innerHTML = 'S. mountains';
+    // navBtn3.onclick = sMtnFunc;
+    // navBtn4.innerHTML = '';
     i = 0;
-    txt = 'You find yourself standing on a dirt road in front of an admittedly odd statue of an angel. The road heads into the mountains to the north and the south. Opposite the statue, a path leads into a large forest to the west.  The rest of this message is just for testing the size of this textbox.  Hopefully it will be large enough to contain everything I need to say.';
+    txt = statueObj.dialogueTxt;
     document.getElementById("dialogueBox").innerHTML = '';
     dialogueFunc();
 }
 
 
 document.onload = statueFunc();
-
-
-// churchBtn.onclick = () => {
-//     dialogueFunc(' church');
-//     bgFunc('church',1,2);
-// }
-
-// estateBtn.onclick = () => {
-//     dialogueFunc('n estate');
-//     bgFunc('estate',1,3);
-// }
-
-// forestBtn.onclick = () => {
-//     dialogueFunc(' forest');
-//     bgFunc('forest',1,6);
-// }
-
-// gardenBtn.onclick = () => {
-//     dialogueFunc(' garden');
-//     bgFunc('garden',1,6);
-// }
-
-// greenhouseBtn.onclick = () => {
-//     dialogueFunc(' greenhouse');
-//     bgFunc('greenhouse',1,3);
-// }
-
-// hutBtn.onclick = () => {
-//     dialogueFunc(' hut');
-//     bgFunc('hut',1,3);
-// }
-
-// lakeBtn.onclick = () => {
-//     dialogueFunc(' lake');
-//     bgFunc('lake',1,7);
-// }
-
-// libraryBtn.onclick = () => {
-//     dialogueFunc(' library');
-//     bgFunc('library',1,3);
-// }
-
-// lighthouseBtn.onclick = () => {
-//     dialogueFunc(' lighthouse');
-//     bgFunc('lighthouse',1,2);
-// }
-
-// mountainBtn.onclick = () => {
-//     dialogueFunc(' mountain');
-//     bgFunc('mountain',1,4);
-// }
-
-// riverBtn.onclick = () => {
-//     dialogueFunc(' river');
-//     bgFunc('river',1,6);
-// }
-
-// towerBtn.onclick = () => {
-//     dialogueFunc(' tower');
-//     bgFunc('tower',1,4);
-// }
